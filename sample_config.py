2ed43ff47e4c182339f4e0f3c2711e10a1def2f3
bsweger <bsweger@gmail.com>
2014-01-05 19:32:13 -0500

    add photo_folder to sample_config

--- sample_config.py
+++ sample_config.py
@@ -1,3 +1,4 @@
 flickr_api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxx' #your Flickr API key
 year = 2006
 username = '' # flickr username
+photo_folder = 'photos' #folder photos will be saved in; must be created before running script
